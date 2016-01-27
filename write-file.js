0634fbccbcd0c66805ba463975fcfccd8c6b6d5f
CJ Apel <cj.e.apel@gmail.com>
2016-01-27 14:54:44 -0800

    Add documentation

--- write-file.js
+++ write-file.js
@@ -8,10 +8,19 @@ if (!filepath) {
   return;
 }
 
+// Initialize write stream
 var ws = fs.createWriteStream(__dirname + '/' + filepath);
 
+/* 
+  Standard input will emit a 'readable' event when it has data to read
+  and adding this listener puts the terminal into interactive mode
+*/
 process.stdin.on('readable', function(){
+  
+  // Read the data the user gives
   var chunk = process.stdin.read();
+  
+  // Guard against null chunk (calling toSting().toLowerCase() on null will break the program 
   if (chunk) {
     // Check for the end command to terminate stdin stream
     if (chunk.toString().toLowerCase() === "end\n") {
