c0c1987f0aba2b54aa2ace11bd5593dbb78f2476
CJ Apel <cj.e.apel@gmail.com>
2016-01-27 14:50:01 -0800

    Add documentation

--- greet.js
+++ greet.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 var fs = require('fs');
+
+// Create a new write stream at the desired location
 var out = fs.createWriteStream(__dirname + '/new-file.txt');
-var arg = process.argv[2];
-out.write("Hello," + process.env.USER + ". You wrote:\n" + process.argv.slice(2).join(" ") + "\n");
+
+// Grab all arguments from user input. Relevant arguments start at index 2
+var args = process.argv.slice(2);
+out.write("Hello," + process.env.USER + ". You wrote:\n" + args.join(" ") + "\n");
+
+// Close the stream
 out.end();
