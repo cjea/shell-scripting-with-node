71915212f5e1f03544c89396fd59c5cd6e25afd6
CJ Apel <cj.e.apel@gmail.com>
2016-01-27 15:04:23 -0800

    Add documentation

--- log-args.js
+++ log-args.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
 console.log(process.argv);
+
+/*
+  Line 1 tells our machine to execute this file using node.
+  The process variable on line 2 is exposed as a global variable in node, 
+  and it gives us access to important information from our machine, including
+  standard input/output/error streams, $USER, $PATH, argv, and much more.
+  The argv property of the process object contains an array of command line 
+  arguments. The first argument will be 'node', the second will be
+  the name of the Javascript file we're working with, and the remaining args 
+  will be those that the user put in. Access those args with process.argv.slice(2)
+*/
